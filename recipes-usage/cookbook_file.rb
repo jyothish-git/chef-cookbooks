ea335c46936e8ddc7f35dff44210332e7f5ffa81
Jyothish <jyothishkumars007@gmail.com>
2021-05-16 21:15:14 +0530

    syntax correction

--- recipes-usage/cookbook_file.rb
+++ recipes-usage/cookbook_file.rb
@@ -1,7 +1,7 @@
 #Usage of Chef resource cookbook_file
 
-cookbook_file "/usr/sbin/smartmon.sh" do
-	source "smartmon.sh"
+cookbook_file "/usr/sbin/smart.sh" do
+	source "smart.sh"
 	mode '0755'
 	owner 'root'
 	group 'root'
